5b54b871036ec2a859c170628b59a850b917a90c
정형진 <88423849+KeMezz@users.noreply.github.com>
2022-09-19 00:50:54 +0900

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from "styled-components";
+import NavBar from "./components/NavBar";
+import "./styles/reset.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <NavBar />
+    </Container>
   );
 }
+
+const Container = styled.main`
+  max-width: 1200px;
+  margin: 0 auto;
+  background-color: #333;
+  color: #fff;
+`;
 
 export default App;
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+function NavBar() {
+  return <Nav></Nav>;
+}
+
+const Nav = styled.nav`
+  width: 100px;
+  height: 100%;
+`;
+
+export default NavBar;
