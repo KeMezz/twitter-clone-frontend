d5ce80fcc5d97ec42b62b4b02f20f89f7fbbd25c
정형진 <88423849+KeMezz@users.noreply.github.com>
2022-09-25 21:46:51 +0900

    implement update and delete tweets

--- src/components/TweetCard.jsx
+++ src/components/TweetCard.jsx
@@ -1,27 +1,81 @@
 import styled from "styled-components";
+import { callAPI } from "../utils/axios";
+import { useSetRecoilState } from "recoil";
+import { globalOverlayStatus } from "../utils/recoils";
+import { useRef, useState } from "react";
 
 function TweetCard({ id, text, createdAt, url, username, setRerender }) {
+  const [updateMode, setUpdateMode] = useState(false);
+  const setGlobalOverlay = useSetRecoilState(globalOverlayStatus);
+  const inputRef = useRef();
+
   const deleteTweet = async () => {
-    const response = await fetch(`http://localhost:8080/tweets/${id}`, {
-      method: "DELETE",
-    });
-    if (response.status === 204) {
-      setRerender((prev) => prev + 1);
-    }
+    callAPI
+      .delete(`/tweets/${id}`)
+      .then(({ data }) => {
+        console.log(data);
+      })
+      .catch((error) => {
+        console.error(error);
+        setGlobalOverlay({
+          status: true,
+          title: "트윗 삭제 실패",
+          message: error.message,
+        });
+      });
+    setRerender((prev) => prev + 1);
+  };
+
+  const switchToUpdateMode = () => setUpdateMode(true);
+  const endUpdateMode = () => setUpdateMode(false);
+
+  const updateTweet = () => {
+    callAPI
+      .put(`/tweets/${id}`, { text: inputRef.current.value })
+      .then((response) => {
+        console.log(response);
+        setUpdateMode(false);
+        setRerender((prev) => prev + 1);
+      })
+      .catch((error) => {
+        console.error(error);
+        setGlobalOverlay({
+          isOpen: true,
+          title: "트윗 수정 실패",
+          message: error.message,
+        });
+      });
   };
 
   return (
     <CardContainer>
       <Buttons>
-        <button>수정</button>
-        <button onClick={deleteTweet}>삭제</button>
+        {updateMode ? (
+          <>
+            <Btn onClick={endUpdateMode}>취소</Btn>
+            <Btn onClick={updateTweet}>수정 완료</Btn>
+          </>
+        ) : (
+          <>
+            <Btn color="blue" onClick={switchToUpdateMode}>
+              수정
+            </Btn>
+            <Btn onClick={deleteTweet} color="crimson">
+              삭제
+            </Btn>
+          </>
+        )}
       </Buttons>
       <Profile>
         <ProfileImg src={url}></ProfileImg>
       </Profile>
       <Message>
         <Username>@{username}</Username>
-        {text}
+        {updateMode ? (
+          <input type="text" defaultValue={text} ref={inputRef} />
+        ) : (
+          text
+        )}
         <CreatedAt>
           {new Date(createdAt).toLocaleDateString()}에 작성됨.
         </CreatedAt>
@@ -43,14 +97,18 @@ const Buttons = styled.div`
   gap: 8px;
   position: absolute;
   right: 12px;
-  button {
-    cursor: pointer;
-  }
+`;
+const Btn = styled.button`
+  cursor: pointer;
+  background-color: transparent;
+  border: none;
+  font-size: 12px;
+  color: ${({ color }) => color};
 `;
 const Profile = styled.div`
-  width: 70px;
-  height: 70px;
-  border-radius: 35px;
+  width: 60px;
+  height: 60px;
+  border-radius: 30px;
   background-color: ${({ theme }) => theme.cardColor};
   overflow: hidden;
 `;
@@ -61,6 +119,9 @@ const Message = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
+  /* width: calc(100% - 60px); */
+  line-height: 1.4;
+  width: 80%;
   gap: 10px;
 `;
 const Username = styled.p`
