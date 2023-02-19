e2ffcc2ef7e6c74d0b572574d0bff0e37eec1b54
정형진 <88423849+KeMezz@users.noreply.github.com>
2023-02-19 16:16:48 +0900

    Create User.jsx

---
+++ src/pages/User.jsx
@@ -0,0 +1,43 @@
+import { useEffect } from "react";
+import { useState } from "react";
+import { useSetRecoilState } from "recoil";
+import { callAPI } from "../utils/axios";
+import { globalOverlayStatus } from "../utils/recoils";
+import TweetsContainer from "../components/TweetContainer";
+import { useParams } from "react-router-dom";
+
+const User = () => {
+  const [tweets, setTweets] = useState([]);
+  const [rerender, setRerender] = useState(0);
+  const { username } = useParams();
+  console.log(username);
+  const setGlobalOverlay = useSetRecoilState(globalOverlayStatus);
+
+  useEffect(() => {
+    const controller = new AbortController();
+    callAPI
+      .get(`/tweets?username=${username}`, { signal: controller.signal })
+      .then(({ data }) => {
+        console.log(data);
+        setTweets(data);
+      })
+      .catch((error) => {
+        console.log(error);
+        if (error.message !== "canceled") {
+          setGlobalOverlay({
+            isOpen: true,
+            title: "트윗 불러오기 실패",
+            message: error.message,
+            buttonClose: true,
+          });
+        }
+      });
+    return () => {
+      controller.abort();
+    };
+  }, [setGlobalOverlay, rerender, username]);
+
+  return <TweetsContainer setRerender={setRerender} tweets={tweets} />;
+};
+
+export default User;
