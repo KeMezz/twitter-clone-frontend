44229269a53fe7c50b6485a5bf89038a4c12e9f6
정형진 <88423849+KeMezz@users.noreply.github.com>
2023-03-13 21:51:55 +0900

    스토리지 참조하여 헤더에 토큰 첨부

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -2,6 +2,10 @@ import axios from "axios";
 
 axios.defaults.baseURL = process.env.REACT_APP_API_SERVER;
 
+if (localStorage.getItem("token")) {
+  axios.defaults.headers["Authorization"] = localStorage.getItem("token");
+}
+
 export const callAPI = axios.create({
   headers: { "Content-type": "application/json; charset=UTF-8" },
   timeout: 1000,
