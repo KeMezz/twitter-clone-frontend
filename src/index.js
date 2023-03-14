412b13a27cc0ff2dca65ae21b3598de57e76ab58
정형진 <88423849+KeMezz@users.noreply.github.com>
2023-03-14 22:34:53 +0900

    socket.io 설정

--- src/index.js
+++ src/index.js
@@ -2,8 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { RecoilRoot } from "recoil";
+import socket from "socket.io-client";
 
+const BASE_URL = process.env.REACT_APP_API_SERVER;
 const root = ReactDOM.createRoot(document.getElementById("root"));
+export const socketIO = socket(BASE_URL);
+
+socketIO.on("connect_error", (error) => {
+  console.log("socket error", error);
+});
+
 root.render(
   <React.StrictMode>
     <RecoilRoot>
