c1c9ba93decb38395d26cf9035fa551fa97ecfa4
정형진 <88423849+KeMezz@users.noreply.github.com>
2023-08-01 12:20:56 +0900

    타임아웃 시간 변경
    1초에서 30초로 변경

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -8,6 +8,6 @@ if (localStorage.getItem("token")) {
 
 export const callAPI = axios.create({
   headers: { "Content-type": "application/json; charset=UTF-8" },
-  timeout: 1000,
+  timeout: 30000,
   timeoutErrorMessage: "서버와 통신할 수 없습니다.",
 });
